f28868f4ddb84d52585a1a7d699b09e37888a93b
Michal Dyzma <mdyzma@wp.pl>
2019-07-11 23:15:19 +0200

    Logic comparing image passed with request

--- face/compare.py
+++ face/compare.py
@@ -1,1 +1,30 @@
 import face_recognition
+import numpy as np
+
+
+def picture_to_bytes(picture, jitters=1):
+    picture_to_store = face_recognition.load_image_file(picture)
+    face_encoding = face_recognition.face_encodings(
+        picture_to_store, num_jitters=jitters)
+    return face_encoding[0].tobytes()
+
+
+def compare_features(unknown_features, known_features, tolerance=0.7):
+    result = face_recognition.compare_faces(known_features,
+                                            unknown_features,
+                                            tolerance=tolerance)
+
+    return result
+
+
+def verify_user(unknown, known_users):
+    for _, val in known_users.items():
+        known = val.get('face_features')
+        known = np.frombuffer(known)
+        try:
+            if compare_features(unknown, known):
+                return val
+            else:
+                return {'error': 'unknown user'}
+        except ValueError:
+            return {'error': 'Use RGB pictures'}
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import flask
-from flask import Flask, request, jsonify
-# from flask_restful import Resource, Api
+import face_recognition
+from flask import Flask, request, jsonify, render_template
+
+from bob_assistant.face.data import FEATURES
+from bob_assistant.face.compare import picture_to_bytes, \
+    verify_user
+
 
 app = Flask(__name__)
-# api = Api(app)
 
 
 @app.route('/')
@@ -11,21 +15,17 @@ def hello():
     """Return a friendly HTTP greeting."""
     return '<h1>Hello Netgural!</h1>'
 
-# class HelloWorld(restful.Resource):
-#     def get(self):
-#         return {'hello': 'Michal'}
 
-
-# api.add_resource(HelloWorld, '/upload')
-
-@app.route('/upload', methods=['POST'])
-def upload():
+@app.route('/verify', methods=['POST'])
+def verify():
     if request.method == 'POST':
         if not 'file' in request.files:
             return jsonify({'error': 'no file'}), 400
+
         img_file = request.files.get('file')
-        img_name = img_name = img_file.filename
-        return img_name
+        img_name = img_file.filename
+        img_file_encoded = face_recognition.face_encodings(img_file)
+        return verify_user(img_file_encoded, FEATURES)
 
 
 @app.errorhandler(404)
