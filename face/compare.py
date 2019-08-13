07a4203bfe6426438588d84f7fe18c59b545b389
Michal Dyzma <mdyzma@wp.pl>
2019-08-13 09:33:52 +0200

    switch to keras-vggface lib

--- face/compare.py
+++ face/compare.py
@@ -1,20 +1,64 @@
-import face_recognition
+from PIL import Image
 import numpy as np
+from numpy import expand_dims
+from numpy import asarray
+from scipy.spatial.distance import cosine
+from mtcnn.mtcnn import MTCNN
+import matplotlib.pyplot as plt
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
 
 
-def picture_to_bytes(picture, jitters=1):
-    picture_to_store = face_recognition.load_image_file(picture)
-    face_encoding = face_recognition.face_encodings(
-        picture_to_store, num_jitters=jitters)
-    return face_encoding[0].tobytes()
+def extract_face(filename, required_size=(224, 224)):
+    # load image from file
+    pixels = plt.imread(filename)
+    # create the detector, using default weights
+    detector = MTCNN()
+
+    results = detector.detect_faces(pixels)
+    # extract the bounding box from the first face
+    x1, y1, width, height = results[0]['box']
+    x2, y2 = x1 + width, y1 + height
+    # extract the face
+    face = pixels[y1:y2, x1:x2]
+    # resize pixels to the model size
+    image = Image.fromarray(face)
+    image = image.resize(required_size)
+    face_array = asarray(image)
+    return face_array
+
+# extract faces and calculate face embeddings for a list of photo files
 
 
-def verify_user(unknown, known_users, tolerance=0.7):
+def get_embeddings(filenames):
+    # extract faces
+    faces = [extract_face(f) for f in filenames]
+    # convert into an array of samples
+    samples = asarray(faces, 'float32')
+    # prepare the face for the model, e.g. center pixels
+    samples = preprocess_input(samples)
+    # create a vggface model
+    model = VGGFace(include_top=False, input_shape=(224, 224, 3))
+    # perform prediction
+    yhat = model.predict(samples)
+    return yhat
+
+
+def is_match(known_embedding, candidate_embedding, thresh=0.5):
+    # calculate distance between embeddings
+    score = cosine(known_embedding, candidate_embedding)
+    if score <= thresh:
+        return True
+    else:
+        return False
+
+
+def verify_user(unknown, known_users, tolerance=0.5):
     for _, val in known_users.items():
         known = val.get('face_features')
-        known = np.frombuffer(known)
+        # known = np.frombuffer(known)
         try:
-            if face_recognition.compare_faces(known, unknown, tolerance=tolerance)[0]:
+            if is_match(known, unknown, tolerance=tolerance):
                 return val
             else:
                 return {'error': 'unknown user'}
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import flask
-import face_recognition
-from flask import Flask, request, jsonify, render_template
+import matplotlib.pyplot as plt
+from io import BytesIO
 
-from bob_assistant.face.data import FEATURES
-from bob_assistant.face.compare import verify_user
+from flask import Flask, request, jsonify, render_template
+from face.compare import get_embeddings, verify_user
 
 
 app = Flask(__name__)
@@ -15,28 +15,22 @@ def hello():
     return '<h1>Hello Netgural!</h1>'
 
 
-@app.route('/verify', methods=['POST'])
+@app.route('/verify', methods=['GET', 'POST'])
 def verify():
+
+    if request.method == 'GET':
+        return jsonify({'error': 'only POST requests with file'})
+
     if request.method == 'POST':
         if not 'file' in request.files:
             return jsonify({'error': 'no file'}), 400
 
-        img_file = request.files.get('file')
-        img_file_encoded = face_recognition.face_encodings(img_file)
-        return verify_user(img_file_encoded, FEATURES)
+        img_file = BytesIO(request.files.get('file').read())
+        # img_name = img_file.filename
+        face = get_embeddings(img_file)
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
-
-
-@app.errorhandler(500)
-def page_not_found(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+        return jsonify(face)
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8088, debug=True)
